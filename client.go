ef386e3ebf74800583f557baa73cb1a7b17ee8aa
bogdannbv <bogdannbv@gmail.com>
2022-09-06 02:31:10 +0300

    initial commit

---
+++ client.go
@@ -0,0 +1,136 @@
+package scrapeninja
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"sort"
+	"time"
+)
+
+var (
+	BaseUrl                    = "https://scrapeninja.p.rapidapi.com"
+	ContentType                = "application/json"
+	RapidAPIHost               = "scrapeninja.p.rapidapi.com"
+	Timeout      time.Duration = 30
+)
+
+type ScrapeRequest struct {
+	Url     string            `json:"url"`
+	Proxy   string            `json:"geo"`
+	Headers map[string]string `json:"headers"`
+	Method  string            `json:"method"`
+}
+
+func (r *ScrapeRequest) MarshalJSON() ([]byte, error) {
+	keys := make([]string, 0)
+	for header := range r.Headers {
+		keys = append(keys, header)
+	}
+
+	sort.Strings(keys)
+
+	headers := make([]string, 0)
+	for _, header := range keys {
+		headers = append(headers, fmt.Sprintf("%s: %s", header, r.Headers[header]))
+	}
+
+	type ScrapeRequestAlias ScrapeRequest
+	return json.Marshal(&struct {
+		*ScrapeRequestAlias
+		Headers []string `json:"headers"`
+	}{
+		ScrapeRequestAlias: (*ScrapeRequestAlias)(r),
+		Headers:            headers,
+	})
+}
+
+type ScrapeResponse struct {
+	Info struct {
+		Version       string            `json:"version"`
+		StatusCode    int               `json:"statusCode"`
+		StatusMessage string            `json:"statusMessage"`
+		Headers       map[string]string `json:"headers"`
+	} `json:"info"`
+	Body string `json:"body"`
+}
+
+type Client struct {
+	httpClient *http.Client
+	apiKey     string
+}
+
+func (c *Client) newRequest(method string, path string, body io.Reader) (*http.Request, error) {
+	u, _ := url.Parse(BaseUrl)
+	u.Path = path
+
+	request, err := http.NewRequest(method, u.String(), body)
+	if err != nil {
+		return nil, err
+	}
+
+	request.Header.Set("Content-Type", ContentType)
+	request.Header.Set("X-RapidAPI-Host", RapidAPIHost)
+	request.Header.Set("X-RapidAPI-Key", c.apiKey)
+
+	return request, err
+}
+
+func (c *Client) Scrape(request *ScrapeRequest) (*ScrapeResponse, error) {
+	jsonRequest, err := json.Marshal(request)
+	if err != nil {
+		return nil, err
+	}
+
+	scrapeRequest, err := c.newRequest(
+		http.MethodPost,
+		"/scrape",
+		bytes.NewBuffer(jsonRequest),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.httpClient.Do(scrapeRequest)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Print(err)
+		}
+	}(resp.Body)
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		return nil, errors.New(string(body))
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var scrapeResponse ScrapeResponse
+
+	err = json.Unmarshal(body, &scrapeResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return &scrapeResponse, nil
+}
+
+func New(apiKey string) Client {
+	return Client{
+		apiKey:     apiKey,
+		httpClient: &http.Client{Timeout: time.Second * Timeout},
+	}
+}
---
+++ client_test.go
@@ -0,0 +1,169 @@
+package scrapeninja
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestNew(t *testing.T) {
+	apiKey := "test-api-key"
+	timeout := time.Second * Timeout
+
+	client := New(apiKey)
+
+	if client.apiKey != apiKey {
+		t.Errorf("got: %s, wanted: %s", client.apiKey, apiKey)
+	}
+
+	if client.httpClient.Timeout != timeout {
+		t.Errorf("got: %q, wanted: %q", client.httpClient.Timeout, timeout)
+	}
+}
+
+func TestScrapeRequest_MarshalJSON(t *testing.T) {
+	tests := map[string]struct {
+		url     string
+		proxy   string
+		headers map[string]string
+		method  string
+		want    string
+	}{
+		"url is empty": {
+			"",
+			"test-proxy",
+			nil,
+			"GET",
+			"{\"url\":\"\",\"geo\":\"test-proxy\",\"method\":\"GET\",\"headers\":[]}",
+		},
+		"proxy is empty": {
+			"https://test",
+			"",
+			nil,
+			"GET",
+			"{\"url\":\"https://test\",\"geo\":\"\",\"method\":\"GET\",\"headers\":[]}",
+		},
+		"method is empty": {
+			"https://test",
+			"test-proxy",
+			nil,
+			"",
+			"{\"url\":\"https://test\",\"geo\":\"test-proxy\",\"method\":\"\",\"headers\":[]}",
+		},
+		"headers is nil": {
+			"https://test",
+			"test-proxy",
+			nil,
+			"GET",
+			"{\"url\":\"https://test\",\"geo\":\"test-proxy\",\"method\":\"GET\",\"headers\":[]}",
+		},
+		"headers has one element": {
+			"https://test",
+			"test-proxy",
+			map[string]string{"test-header": "test-value"},
+			"GET",
+			"{\"url\":\"https://test\",\"geo\":\"test-proxy\",\"method\":\"GET\",\"headers\":[\"test-header: test-value\"]}",
+		},
+		"headers has multiple elements": {
+			"https://test",
+			"test-proxy",
+			map[string]string{
+				"test-header":  "test-value",
+				"test-header2": "test-value2",
+				"test-header3": "test-value3",
+			},
+			"GET",
+			"{\"url\":\"https://test\",\"geo\":\"test-proxy\",\"method\":\"GET\",\"headers\":[\"test-header: test-value\",\"test-header2: test-value2\",\"test-header3: test-value3\"]}",
+		},
+	}
+
+	for name, data := range tests {
+		t.Run(name, func(t *testing.T) {
+			request := ScrapeRequest{
+				Url:     data.url,
+				Proxy:   data.proxy,
+				Headers: data.headers,
+				Method:  data.method,
+			}
+
+			got, err := request.MarshalJSON()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if string(got) != data.want {
+				t.Errorf("got: %s, wanted: %s", string(got), data.want)
+			}
+		})
+	}
+}
+
+func TestClient_Scrape(t *testing.T) {
+	apiKey := "test-api-key"
+
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		err := json.NewEncoder(w).Encode(&ScrapeResponse{
+			Info: struct {
+				Version       string            `json:"version"`
+				StatusCode    int               `json:"statusCode"`
+				StatusMessage string            `json:"statusMessage"`
+				Headers       map[string]string `json:"headers"`
+			}{
+				"test-version",
+				200,
+				"test-message",
+				nil,
+			},
+			Body: "test-body",
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		t.Run("path is correct", func(t *testing.T) {
+			got := r.URL.Path
+			if got != "/scrape" {
+				t.Errorf("got: %s, wanted: %s", got, "scrape")
+			}
+		})
+
+		t.Run("X-RapidAPI-Key header is set correctly", func(t *testing.T) {
+			got := r.Header.Get("X-RapidAPI-Key")
+			if got != apiKey {
+				t.Errorf("got: %s, wanted: %s", got, apiKey)
+			}
+		})
+
+		t.Run("X-RapidAPI-Host header is set correctly", func(t *testing.T) {
+			got := r.Header.Get("X-RapidAPI-Host")
+			if got != RapidAPIHost {
+				t.Errorf("got: %s, wanted: %s", got, RapidAPIHost)
+			}
+		})
+
+		t.Run("Content-Type header is set correctly", func(t *testing.T) {
+			got := r.Header.Get("Content-Type")
+			if got != ContentType {
+				t.Errorf("got: %s, wanted: %s", got, ContentType)
+			}
+		})
+	}))
+	defer srv.Close()
+
+	// Overwrite base url with test server url
+	BaseUrl = srv.URL
+
+	client := New(apiKey)
+	response, _ := client.Scrape(&ScrapeRequest{
+		Url:     "",
+		Proxy:   "",
+		Headers: nil,
+		Method:  "",
+	})
+
+	if response.Body != "test-body" {
+		t.Errorf("Oh no!")
+	}
+}
